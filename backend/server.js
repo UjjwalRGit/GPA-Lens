a21859e65096bb4ac86d8dcc8be4635b7a80901b
Ujjwal <raghu099@umn.edu>
2025-10-16 10:34:04 -0500

    Fixed SSL Issue

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,10 @@ const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true 
+  }
 };
 
 let database;
